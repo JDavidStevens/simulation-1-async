8637568225d41f54ee22f3618bd1f1565f559d0b
unknown <jdavid.stevens@gmail.com>
2018-08-29 14:39:11 -0600

    829binA_controller

---
+++ shelfie1/server/Acontroller.js
@@ -0,0 +1,58 @@
+module.exports={
+
+//ShelfA
+create: (req,res,next)=>{
+    const dbInstance=req.app.get('db');
+
+    dbInstance.create_binA()
+    .then(product=>res.status(200).send(product))
+    .catch(err=>{
+        res.status(500).send({errorMessage:"Oops! Something went wrong. Our engineers have been informed!"});
+        console.log(err)
+    })
+},
+
+readTable: (req,res,next)=>{
+    const dbInstance=req.app.get('db');
+    
+    dbInstance.read_tableA()
+    .then(products=>res.status(200).send(products))
+    .catch(err=>{
+        res.status(500).send({errorMessage:"Oops! Something went wrong. Our engineers have been informed!"});
+        console.log(err)
+    })
+}, 
+
+readBin:(req,res,next)=>{
+    const dbInstance=req.app.get('db');
+
+    dbInstance.read_bin_tableA()
+    .then(product=>res.status(200).send(product))
+    .catch(err=>{
+        res.status(500).send({errorMessage:"Oops! Something went wrong. Our engineers have been informed!"});
+        console.log(err)
+    })
+},
+
+update:(req,res,next)=>{
+    const dbInstance=req.app.get('db');
+
+    dbInstance.update_binA()
+    .then((product)=>res.status(200).send(product))
+    .catch(err=>{
+        res.status(500).send({errorMessage:"Oops! Something went wrong. Our engineers have been informed!"});
+        console.log(err)
+    })
+},
+
+delete: (req,res,next)=>{
+    const dbInstance= req.app.get('db');
+
+    dbInstance.delete_binA()
+    .then(()=>res.sendStatus(200))
+    .catch(err=>{
+        res.status(500).send({errorMessage:"Oops! Something went wrong. Our engineers have been informed!"});
+        console.log(err)
+    })
+}
+}
