4d90d5d613a6fd9d8666ba419dbe593af38cb1b1
unknown <jdavid.stevens@gmail.com>
2018-09-03 18:41:52 -0600

    93additional_copying

--- shelfie1/src/Shelves/ShelfB.js
+++ shelfie1/src/Shelves/ShelfB.js
@@ -1,17 +1,43 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import axios from 'axios';
 import logo from '../logo.png';
 import './Shelf.css';
 
-export default class ShelfB extends Component{
+export default class ShelfB extends Component{constructor(){
+    super()
+    this.state={
+        bin:[]
+    }
+}
+
+componentDidMount(){
+    axios.get('/api/products_b').then(response=>{
+        this.setState({bin: response.data})
+    })
+}
+
     render(){
+
+        let products=this.state.bin.map((element,index)=>{
+            if(element.product_name===null){
+                return <Link key={index} to ={`/badd/${element.id}`}><button  className="bin-no-inv-button">+ Add Inventory</button></Link>
+              }else{ 
+              return <Link key={index} to = {`/binventory/${element.id}`}><button key="element.id" className="bin-with-inv-button">Bin {element.id}</button></Link>}
+               
+          })
+            
+                
+            
         return(
             <div>
                 <header className='shelf-banner'>
                <Link to ='/'><img src={logo} className='shelf-logo' alt="logo"/></Link>
                 <h1 className="shelf-title">Shelf B</h1>
                 </header>
+                {products}                
             </div>
         )
     }
 }
+
--- shelfie1/src/routes.js
+++ shelfie1/src/routes.js
@@ -2,12 +2,23 @@ import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 
 import Homepage from './Homepage/Homepage';
+
 import ShelfA from './Shelves/ShelfA';
 import ShelfB from './Shelves/ShelfB';
 import ShelfC from './Shelves/ShelfC';
 import ShelfD from './Shelves/ShelfD';
+
 import A_add from './Bins/BinA/A_add';
 import InventoryA from './Bins/BinA/InventoryA';
+
+import B_add from './Bins/BinB/B_add';
+import InventoryB from './Bins/BinB/InventoryB';
+
+import C_add from './Bins/BinC/C_add';
+import InventoryC from './Bins/BinC/InventoryC';
+
+import D_add from './Bins/BinD/D_add';
+import InventoryD from './Bins/BinD/InventoryD';
 
 export default <Switch>
 <Route exact path='/' component={Homepage}/>
@@ -18,5 +29,14 @@ export default <Switch>
 
 <Route path='/aadd/:id' component={A_add}/>
 <Route path='/ainventory/:id' component={InventoryA}/>
+
+<Route path='/badd/:id' component={B_add}/>
+<Route path='/binventory/:id' component={InventoryB}/>
+
+<Route path='/cadd/:id' component={C_add}/>
+<Route path='/cinventory/:id' component={InventoryC}/>
+
+<Route path='/dadd/:id' component={D_add}/>
+<Route path='/dinventory/:id' component={InventoryD}/>
 
 </Switch>
